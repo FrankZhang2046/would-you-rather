ddc636754cc5a4026ec10576dabb8079b5f62b2b
frank zhang <frankbusinessmail@gmail.com>
2019-10-29 12:59:17 -0400

    homescreen questions biforcated

--- src/components/Homescreen.js
+++ src/components/Homescreen.js
@@ -1,19 +1,40 @@
 import React from 'react';
+import '../styles/Homescreen.scss';
 import { connect } from "react-redux";
 
-const Homescreen = props => {
-    return(
-        <div className="homescreen">
-            This is the homescren
-        </div>
-    )
+class Homescreen extends React.Component {
+    state={
+        display: '',
+    }
+
+    render(){
+        return(
+            <div className="homescreen">
+                <div className="homescreen__title">
+                    <p className="homescreen__title--unanswered">UNANSWERED</p>
+                    <p className="homescreen__title--answered">ANSWERED</p>
+                </div>
+            </div>
+        )
+    }
 }
 
 const mapStateToProps = (state, ownProps) => {
     const {questions} = state;
+    let answered = [];
+    let unAnswered = [];
+    answered = Object.keys(questions).filter(
+        question => questions[question].optionOne.votes.length > 0 || questions[question].optionTwo.votes.length > 0
+    );
+
+    unAnswered = Object.keys(questions).filter(
+        question => questions[question].optionOne.votes.length === 0 && questions[question].optionTwo.votes.length === 0
+    );
 
     return {
+        answered,
         questions,
+        unAnswered,
     }
 }
 
