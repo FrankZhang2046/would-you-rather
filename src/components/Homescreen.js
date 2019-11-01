daf487b9d3c4fad4df84af7ad6a6f1bb533b4e30
frank zhang <frankbusinessmail@gmail.com>
2019-11-01 11:29:21 -0400

    eliminated homescreen key error

--- src/components/Homescreen.js
+++ src/components/Homescreen.js
@@ -22,7 +22,7 @@ class Homescreen extends React.Component {
                 <div className="homescreen__display">
                         {
                             this.props[this.state.display].map(
-                                question => <Question id={question} status={this.state.display}/>
+                                question => <Question key={question} id={question} status={this.state.display}/>
                             )
                         }
                     </div>
