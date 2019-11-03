d9de739d369eee7b0cc5a618c7bc16266f7fd582
frank zhang <frankbusinessmail@gmail.com>
2019-11-03 09:22:34 -0500

    able to map user component within login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import { connect } from "react-redux";
+import User from './User';
 
 class Login extends React.Component{
+
     render(){
+        const {userIds} = this.props;
+
         return(
             <div className="login">
-                this is the login page
+                {userIds.map(user => <User id={user} key={user} />)}
             </div>
         )
     }
 }
 
 const mapStateToProps = (state, ownProps) => {
+    const {users} = state;
+    let userIds = [];
+    Object.keys(users).map(user => userIds.push(users[user].id))
+    
     return {
-        
+        userIds,
     }
 }
 
