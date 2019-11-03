0589b38f974cfb1a4d3af3b17dc5defb63a7617c
frank zhang <frankbusinessmail@gmail.com>
2019-11-02 21:44:38 -0400

    able to display voted option

--- src/components/Result.js
+++ src/components/Result.js
@@ -2,14 +2,22 @@ import React from 'react';
 import { connect } from "react-redux";
 
 const Result = props => {
-    const {question} = props;
+    const {question, authedUser} = props;
+    let voted;
+
+    if (question && question.optionOne.votes.includes(authedUser)) {
+        voted = 'optionOne';
+    }
+    else if (question.optionTwo.votes.includes(authedUser)){
+        voted = 'optionTwo';
+    }
 
     return(
         <div className="result__container">Results: 
-            <div className="result__optionOne">
+            <div className="result__optionOne" style={voted === 'optionOne' ? {'background': 'red'} : null}>
                 {question.optionOne.text}
             </div>
-            <div className="result__optionTwo">
+            <div className="result__optionTwo" style={voted === 'optionTwo' ? {'background': 'red'} : null}>
                 {question.optionTwo.text}
             </div>
             <div className="result__optionTwo"></div>
@@ -19,9 +27,11 @@ const Result = props => {
 
 const mapStateToProps = (state, ownProps) => {
     const question = state.questions[ownProps.id];
+    const {authedUser} = state;
 
     return {
         question,
+        authedUser,
     }
 }
 
