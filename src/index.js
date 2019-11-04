954a5db9b1b5fc4e7b7876733cfe9643e2a564d8
frank zhang <frankbusinessmail@gmail.com>
2019-11-04 09:49:11 -0500

    redux-persist package added

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.scss';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/index.scss";
+import App from "./components/App";
 import { createStore } from "redux";
-import reducer from './reducers';
-import middleware from './middleware';
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
+import reducer from "./reducers";
+import middleware from "./middleware";
 import { Provider } from "react-redux";
 
-const store = createStore(reducer, middleware);
+const persistConfig = {
+  key: "root",
+  storage
+};
 
-ReactDOM.render(<Provider store={store}>
-    <App />
-</Provider>, document.getElementById('root'));
+const persistedReducer = persistReducer(persistConfig, reducer);
 
+const store = createStore(persistedReducer, middleware);
+const persistor = persistStore(store);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>,
+  document.getElementById("root")
+);
