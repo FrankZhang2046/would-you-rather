c389186937e1c5fa0d804cc6860e3d3cbfa55c74
frank zhang <frankbusinessmail@gmail.com>
2019-11-04 09:54:10 -0500

    log out functionality implemented

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,30 @@
 import React from 'react';
 import '../styles/Navbar.scss';
+import { setAuthedUser } from "../actions/authedUser";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
-const Navbar = props => {
-    return(
-        <nav className="navbar">
-            <Link to='/' className="navbar__homeTab">Home</Link>
-            <Link to='/add' className="navbar__newQuestion">New Question</Link>
-            <Link to='/leaderboard' className="navbar__leaderboard">Leaderboard</Link>
-            <p className="navbar__username">{
-                props.loggedIn !== undefined ? props.loggedIn.name : 'username'
-            }
-            </p>
-            <Link to='/login' className="navbar__logout">Logout</Link>
-            
-        </nav>
-    )
+class Navbar extends React.Component {
+    logOut = () => {
+        const {dispatch} = this.props;
+        dispatch(setAuthedUser(null))
+    }
+
+    render(){
+        return(
+            <nav className="navbar">
+                <Link to='/' className="navbar__homeTab">Home</Link>
+                <Link to='/add' className="navbar__newQuestion">New Question</Link>
+                <Link to='/leaderboard' className="navbar__leaderboard">Leaderboard</Link>
+                <p className="navbar__username">{
+                    this.props.loggedIn !== undefined ? this.props.loggedIn.name : 'username'
+                }
+                </p>
+                <Link to='#' className="navbar__logout" onClick={this.logOut}>Logout</Link>
+                
+            </nav>
+        )
+    }
 }
 
 const mapStateToProps = (state, ownProps) => {
