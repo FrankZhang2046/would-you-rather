6f2d692d48da15b87b6fcc276a1162e0922d89a0
frank zhang <frankbusinessmail@gmail.com>
2019-11-04 08:43:34 -0500

    login functionality implemented

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { connect } from "react-redux";
+import { Redirect } from "react-router-dom";
 import { setAuthedUser } from "../actions/authedUser";
 import User from './User';
 
 class Login extends React.Component{
+    state={loggedIn: false}
+
     changeAuthedUser= userId =>{
         const {dispatch} = this.props;
-        dispatch(setAuthedUser(userId))
+        dispatch(setAuthedUser(userId));
+        this.setState({loggedIn: true})
     }
 
     render(){
@@ -14,6 +18,8 @@ class Login extends React.Component{
 
         return(
             <div className="login">
+                {this.state.loggedIn ? <Redirect to='/'/> : null}
+                <div className="login__prompt">Please click on a profile to log in</div>
                 {userIds.map(user => <User id={user} key={user} display={'login'} click={this.changeAuthedUser}/>)}
             </div>
         )
